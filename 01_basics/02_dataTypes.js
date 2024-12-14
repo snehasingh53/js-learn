997a2fd75c1495d0f4669038dcc76e21716e1b45
sneha singh <147160763+snehasingh53@users.noreply.github.com>
2024-12-14 09:38:24 +0000

    converions learnt

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,21 @@
+"use strict"; // treat all JS code as newer version
+
+//alert(3+3) // not allowed in code
+
+let name="sneha" //string
+age=18 //number
+let isLoggedIn = false // boolean 
+
+// number => 2 to power 53 
+//bigint 
+//string => ""
+// boolean => true/false
+//null => standalone value
+//undefined => value not assigned
+//symbol=> unique 
+
+// object 
+
+console.log(typeof "sneha");
+console.log(typeof null ); // object
+console.log(typeof undefined) // undefined
