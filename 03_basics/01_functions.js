bc98c441867fe66916a5956790d7f8871b04e098
sneha singh <147160763+snehasingh53@users.noreply.github.com>
2025-01-01 10:19:41 +0000

    functions done

--- 03_basics/01_functions.js
+++ 03_basics/01_functions.js
@@ -27,3 +27,33 @@ function loginUserMessage(username = "sam"){
 }
 console.log(loginUserMessage("sneha"))  
 console.log(loginUserMessage())// if no value passed then undefined
+
+
+// ... = rest and spread operator depending upon use
+/*function calculateCartPrice(...num1){
+ return num1
+} 
+console.log(calculateCartPrice(200, 400, 600, 150 )) 
+*/
+function calculateCartPrice(val1, val2,...num1){
+    return num1
+   } 
+   console.log(calculateCartPrice(200, 400, 600, 150 ))
+
+
+const user={
+    username:"sneha",
+    price:999
+}
+function handleObject(anyobject){
+    console.log(`Username is ${anyobject.username} and price is ${anyobject.price }`)
+}
+handleObject(user)
+
+
+const myNewArray =[200,400,600,800]
+function returnSecondValue(getArray){
+return getArray[2]
+}
+console.log(returnSecondValue(myNewArray))
+console.log(returnSecondValue([200,400,600,800]))
